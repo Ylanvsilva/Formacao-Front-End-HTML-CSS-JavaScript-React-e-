5328b719d7be0cfe804d1f99b4a5ffc8c9557c2c
Ylan <ylan_vinicio@hotmail.com>
2024-05-10 06:31:55 -0300

    Inicio das aulas de JS

---
+++ 14-OPERADORES-JS/js/script.js
@@ -0,0 +1,64 @@
+// 1 - NUmber
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 -Operações artméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+
+console.log(5 + (4 * 2));
+
+// 3 - Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log(`13`);
+
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`);
+
+// 5 - Símbolos especiais em string
+console.log("Testando a \nquebra de linha");
+
+console.log("Espaçamento de \t de tab");
+
+// 6 - Concatenação
+console.log("Oi," + " tudo" + " bem");
+
+console.log(`Testando ` + `com ` + `crase!`);
+
+// 7 - Template strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - Boolean
+console.log(true);
+
+console.log(5 > 20);
+
+console.log(30 > 10);
+
+console.log(typeof false);
+
+// 9 - Comparações
+console.log(5 <= 5);
+
+console.log(5 < 5);
+
+console.log(10 == 10);
+
+console.log(10 == 9);
+
+console.log(10 != 9);
