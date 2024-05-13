a363434d6d1c82f9f8e27c33d6e2b0e7b2ee4450
Ylan <ylan_vinicio@hotmail.com>
2024-05-13 06:30:01 -0300

    Inicio das estruturas de JS

---
+++ 15-ESTRUTURAS-JS/js/script.js
@@ -0,0 +1,68 @@
+// 1 - variáveis
+let nome = "Matheus";
+
+console.log(nome);
+
+nome = "Matheus Battisti";
+
+confirm.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32;
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - mais sobre variaveis
+// let 2teste = "invalido";
+// let @teste = "invalido";
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+
+const nomeCompleto = "Joao da Silva";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "Ok";
+
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// 3 - prompt
+const age = prompt("Digite a sua idade: ");
+
+console.log(`Você tem ${age} anos.`);
+
+// 4 - alert
+alert("Testando");
+
+const z = 10;
+
+alert(`O numero é ${z}`);
+
+// 5 - Math
+console.log(Math.max(5, 2, 1, 10));
+
+console.log(Math.floor(5.14));
+
+console.log(Math.ceil(5.14));
+
+// 6 - console
+console.log("teste!");
+
+console.error("erro!");
+
+console.warn("aviso!");
+
+// 7 - \
